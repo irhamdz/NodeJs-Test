037b468fcb3636cda642838c19d90b7b60249222
Irham Dzuhri <irhamdz@gmail.com>
2021-03-09 12:33:46 +0700

    add logic test

---
+++ 4-Logic-Test.js
@@ -0,0 +1,12 @@
+function anagram(arr) {
+    result = []
+    if (arr.length < 0 || typeof(arr) != Array) {
+        return result
+    } else {
+        let index;
+        let arrLength = arr.length
+        for (index = 0; index < arrLength; index++) {
+            
+        }
+    }
+}
